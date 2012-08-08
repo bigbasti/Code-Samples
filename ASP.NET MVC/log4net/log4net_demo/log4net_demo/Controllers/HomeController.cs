ae8a176b52e3673f9ce67f1c794528933ad6e25f
Sebastian Gross <bigbasti@gmail.com>
2012-08-08 17:26:34 +0200

    Log4Net Demo zum Blogbeitrag

---
+++ ASP.NET MVC/log4net/log4net_demo/log4net_demo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+
+namespace log4net_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ILog logger = LogManager.GetLogger(typeof(HomeController));
+
+            logger.Info("Index Action wurde aufgerufen.....");
+
+            try
+            {
+                logger.Debug("Don't mess with math dude...");
+                int zero = 0;
+                int endOfWorld = 12 / zero;
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Ohh noez, you're dead now!", ex);
+            }
+
+            return View();
+        }
+
+    }
+}
